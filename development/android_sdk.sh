570593e8ef6b3950612951cde00a0981fa797622
Xennis <xennis@online.de>
2014-02-13 23:41:38 +0100

    improve

--- development/android_sdk.sh
+++ development/android_sdk.sh
@@ -2,17 +2,19 @@
 # --------------------------- Settings ----------------------------
 ANDROID_SKD_INSTALL_DIR=~/Downloads
 PATH_FILE=~/.bashrc
+#PATH_FILE=~/test
 
 # --------------------------- Functions ----------------------------
 function install() {
 	local android_sdk_tar="android-sdk_r22.3-linux.tgz"
 
 	echo ""
-	echo "Download:"
+	echo "Install:"
+	echo "    openjdk-7-jdk"
 	echo "    ${android_sdk_tar}"
 	if ask ">>>>>>>>>>>>>>>>>>>>> Install it?";
 	then	
-#		sudo apt-get install openjdk-7-jdk
+		sudo apt-get install openjdk-7-jdk
 
 		# Download and unzip tar, delete tar
 		if ! check_dir_exits ${ANDROID_SKD_INSTALL_DIR};
@@ -35,6 +37,11 @@ function config() {
 		echo "Backup config file: ${PATH_FILE}.original"
 		sudo cp ${PATH_FILE} ${PATH_FILE}.original
 		echo "" >> ${PATH_FILE}
-		echo "export PATH=${PATH}:${ANDROID_SKD_INSTALL_DIR}/android-sdk-linux/tools:${ANDROID_SKD_INSTALL_DIR}/android-sdk-linux/platform-tools" >> ${PATH_FILE}
+		echo "# Add Android SKD to PATH" >> ${PATH_FILE}
+		echo "export ANDROID_HOME=\"${ANDROID_SKD_INSTALL_DIR}/android-sdk-linux/tools\"" >> ${PATH_FILE}
+		echo "export ANDROID_PLATFORM_TOOLS=\"${ANDROID_SKD_INSTALL_DIR}/android-sdk-linux/platform-tools\"" >> ${PATH_FILE}
+		echo "export PATH=\"\$PATH:\$ANDROID_HOME:\$ANDROID_PLATFORM_TOOLS\"" >> ${PATH_FILE}
+		# Reload
+		source ${PATH_FILE}
 	fi
 }
--- development/phonegap.sh
+++ development/phonegap.sh
@@ -9,11 +9,14 @@ function install() {
 	echo "    nodejs (Node.js)"
 	echo "    nodejs-legacy (https://github.com/joyent/node/issues/3911)"
 	echo "    npm (package manager for Node.js)"
+	echo "	  ant"
 	echo "    phonegap"
+	echo "    cordova"
 	if ask ">>>>>>>>>>>>>>>>>>>>> Install it?";
 	then	
-		sudo apt-get install nodejs nodejs-legacy npm
+		sudo apt-get install nodejs nodejs-legacy npm ant
 		sudo npm install -g phonegap
+		sudo npm install -g cordova
 	fi
 }
 
